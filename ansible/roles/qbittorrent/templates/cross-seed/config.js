f4bc174d8737b2f9894ecb4c2cf889ea12c42117
Mike Wilson <contact@mjwilson.org>
2023-02-22 13:25:15 -0500

    Templatized cross-seed configuration (config.js)

---
+++ ansible/roles/qbittorrent/templates/cross-seed/config.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+module.exports = {
+    delay: 10,
+
+    torznab: [
+	    "http://prowlarr.local.{{ personal_domain }}/1/api?apikey={{ prowlarr_api_key }}", // TorrentLeech
+	    "http://prowlarr.local.{{ personal_domain }}/2/api?apikey={{ prowlarr_api_key }}", // DigitalCore
+	    "http://prowlarr.local.{{ personal_domain }}/3/api?apikey={{ prowlarr_api_key }}", // ImmortalSeed
+	    "http://prowlarr.local.{{ personal_domain }}/4/api?apikey={{ prowlarr_api_key }}", // TorrentSeeds
+	    "http://prowlarr.local.{{ personal_domain }}/5/api?apikey={{ prowlarr_api_key }}", // AnimeTosho
+	    "http://prowlarr.local.{{ personal_domain }}/6/api?apikey={{ prowlarr_api_key }}", // Aither
+    ],
+
+    torrentDir: "/torrents",
+    outputDir: "/cross-seeds",
+    includeEpisodes: false,
+    includeNonVideos: false,
+    fuzzySizeThreshold: 0.02,
+    excludeOlder: undefined,
+    excludeRecentSearch: undefined,
+    action: "inject",
+    rtorrentRpcUrl: undefined,
+    qbittorrentUrl: "https://qbittorrent.local.{{ personal_domain }}",
+    duplicateCategories: true,
+    notificationWebhookUrl: undefined,
+    port: 2468,
+    rssCadence: "2w",
+    searchCadence: "6w",
+};
